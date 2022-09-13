292ae1b100f1d273652c6929bce9d0ea89a7bcf8
LSC-cong <lsc_learn@163.com>
2022-09-13 22:55:58 +0800

    commit1

---
+++ com/src/Test1.java
@@ -0,0 +1,20 @@
+public class Test1 {
+    private String s;
+    private String t;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public void setT(String t) {
+        this.t = t;
+    }
+}
